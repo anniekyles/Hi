a0cb7cfae25097110f641922c68f763d486b0007
unknown <trung.ly@43493-win.auckland.natcoll.ac.nz>
2013-04-19 10:33:32 +1200

    Add game.php

---
+++ game.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once("includes/bag.php");
+    require_once("includes/silo.php");
+    require_once("includes/horse.php");
+    
+    //start the game
+    $oBag = new Bag();
+    $oSilo = new Silo();
+    $oHorse = new Horse();
+    
+    //make your moves
+    
+    
+
+
+?>
